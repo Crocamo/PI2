cd50c4066c2490d01966a60e2e4f3ab2f494f591
André Luiz Crocamo <88804840+Crocamo@users.noreply.github.com>
2023-09-17 15:33:21 -0300

    Add files via upload

---
+++ js/controle.js
@@ -0,0 +1,10 @@
+hab = {
+    for:10,
+    des:10,
+    con:10,
+    int:10,
+    sab:10,
+    car:10
+}
+
+console.log(hab.for);
---
+++ js/script.js
@@ -0,0 +1,12 @@
+function alfabeto(x){
+    let letra = document.querySelector('.pLetra');
+    let Imagem = document.querySelector('.pImagem');
+    let MaoL = document.querySelector('.pMaoL');
+    let MaoP = document.querySelector('.pMaoP');
+    let Som = document.querySelector('.Som');
+
+    letra.textContent = x;
+    Imagem.setAttribute('src', 'img/'+x+'/img.gif')
+    MaoL.setAttribute('src', 'img/'+x+'/Mao_L.gif')
+    MaoP.setAttribute('src', 'img/'+x+'/Mao_P.gif')
+}
